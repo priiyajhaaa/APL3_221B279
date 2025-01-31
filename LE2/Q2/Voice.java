7dcc3ab19a8c28e71d4976128657cd0e3c197c6e
221B279 <221B279@juet.co.in>
2025-01-31 14:37:04 +0530

    Adding LE2

--- LE2/Q2/Voice.java
+++ LE2/Q2/Voice.java
@@ -1,11 +1,13 @@
 class Voice {
-    Animal[] animal =new Animal[5];
     public void prepareVoice(){
+        Animal[] animal =new Animal[5];
+        //{"cow", "dog", "pig","goat","lion"};
         animal[0] = new Cow();
         animal[1] = new Dog();
         animal[2] = new Pig();
         animal[3] = new Goat();
         animal[4] = new Lion();
+        hear(animal);
     }
     public void hear(Animal[] animal){
         for(int i = 0 ; i<5 ; i++){
