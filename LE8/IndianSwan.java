b86d27da881f159ccfa022dd98f71576f6dee128
221B279 <221B279@juet.co.in>
2025-05-09 15:40:04 +0530

    Adding LE8

---
+++ LE8/IndianSwan.java
@@ -0,0 +1,11 @@
+public class IndianSwan implements Swan{
+    public void swim(){
+        System.out.println("Swims!");
+    }
+    public void sing(){
+        System.out.println("Sings good.");
+    }
+    public void eat(){
+        System.out.println("Eats pearls!");
+    }
+}
